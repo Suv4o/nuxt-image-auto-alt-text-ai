c46f0a263a2f4ac2155aaf4f389ae6517fc4333d
Aleks <aleksandar.trpkovski@gmail.com>
2023-11-16 21:19:49 +1100

    Rename the module file

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,5 +1,5 @@
 export default defineNuxtConfig({
-  modules: ["../src/altCraft"],
+  modules: ["../src/module"],
   altCraft: {
     modelName: process.env.MODEL_NAME,
     accessToken: process.env.ACCESS_TOKEN,
